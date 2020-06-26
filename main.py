7ed7e950c7b41e97037c3bd81b3ef861633eb156
MockingJay <58279279+j-tesla@users.noreply.github.com>
2020-06-26 19:51:25 +0530

    Removed settings from main
    As a separate settings.py exists just imported * from it

--- main.py
+++ main.py
@@ -6,26 +6,10 @@
 import random
 import sys
 from heapq import heappush, heappop
-from os import path
 
 import pygame
 
-WIDTH = 480
-HEIGHT = 600
-FPS = 60
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-
-# set up assets folders
-game_dir = path.dirname(__file__)
-img_dir = path.join(game_dir, 'img')
-snd_dir = path.join(game_dir, 'snd')
+from settings import *
 
 # initialise pygame and create window
 pygame.init()
